fae46d8f12746872e1783e68c53cbd00385ed880
Alec Bayliff <bayliffan@gmail.com>
2025-07-08 17:08:21 -0500

    Added working pruning function

--- GPSearch/tree.py
+++ GPSearch/tree.py
@@ -4,6 +4,7 @@ import scipy as sp
 import sys
 from scipy.spatial.distance import cityblock
 from PrettyPrint import PrettyPrintTree
+import copy
 
 class PacTree:
     norder = 0
@@ -16,12 +17,12 @@ class PacTree:
         return self._root
     
     def grow(self,node,depth,mdepth,prob,order):
-        #If at max depth, set nonterminal children
+        #If at max depth, set terminal children
         if depth == mdepth:
             children = []
             for i in range(self._size):
                 self.norder += 1
-                children.append(self.Node(op=self.select_op_t(),children=[],depth=depth,order=self.norder))
+                children.append(self.Node(op=self.select_op_t(),children=[],depth=depth+1,order=self.norder))
             node.set_children(children)
         #Otherwise, grow stochastically
         else:
@@ -36,15 +37,63 @@ class PacTree:
                 children = []
                 for i in range(self._size):
                     self.norder += 1
-                    children.append(self.Node(op=self.select_op_t(),children=[],depth=depth,order=self.norder))
+                    children.append(self.Node(op=self.select_op_t(),children=[],depth=depth+1,order=self.norder))
             else:
                 for i in range(count):
                     self.norder += 1
-                    child = self.Node(op=self.select_op_nt(),children=[],depth=depth,mdepth=mdepth,order=self.norder)
+                    child = self.Node(op=self.select_op_nt(),children=[],depth=depth+1,mdepth=mdepth,order=self.norder)
                     self.grow(child,depth+1,mdepth,prob,self.norder)
                     children.append(child)
             node.set_children(children)
-        
+            
+    def prune(self,node,select):
+        if node.get_order() == select:
+            self.delete_nodes(node)
+            return True
+        else:
+            children = node.get_children()
+            pruned = False
+            if children:
+                for child in node.get_children():
+                    if self.prune(child,select):
+                        children = node.get_children()
+                        for c in range(len(children)):
+                            if children[c].get_order() == select:
+                                pruned = c
+                        children.pop(pruned)
+
+                #If there are not enough children for a nonterminal operation, move child up.
+                if self.check_terminal(node) == False and len(children) < 2:
+                    node.set_operator(children[0].get_operator())
+                    for child in children:
+                        if child.get_children():
+                            node.set_children(child.get_children())
+                    self.norder = 0
+                    self.update_order(self.get_root())
+
+                            
+    def delete_nodes(self,node):
+        children = node.get_children()
+        if children:
+            for child in children:
+                self.delete_nodes(child)
+            children.clear()
+            
+    def update_order(self,node):
+        node.set_order(self.norder)
+        children = node.get_children()
+        if children:
+            for child in children:
+                self.norder += 1
+                self.update_order(child)
+            
+    def check_terminal(self,node):
+        terminals = {'ghost','pill','walls','fruit'}
+        if node.get_operator in terminals:
+            return True
+        else:
+            return False
+    
     def select_op_nt(self):
         rnum = np.random.randint(0,5)
         match rnum:
@@ -87,11 +136,28 @@ class PacTree:
                 return False
             
         def set_children(self,children):
+            self._children = []
             for child in children:
                 self._children.append(child)
+                
+        def set_operator(self,operator):
+            self._operator = operator
+            
+        def set_order(self,order):
+            self._order = order
+                
+        def get_order(self):
+            return self._order
+        
+        def get_operator(self):
+            return self._operator
+        
+        def copy(self,node):
+            return copy.deepcopy(node)
+            
 
 def test_tree():
-    test = PacTree(mdepth=3,size=2,prob=.10)
+    test = PacTree(mdepth=3,size=2,prob=0)
     return test
 
 x = test_tree()
@@ -99,8 +165,10 @@ x = test_tree()
 z = x.get_root()
 pt = PrettyPrintTree(lambda z: z._children, lambda z: z._order)
 pt(x.get_root())
-#pt = PrettyPrintTree(lambda z: z._children, lambda z: z._operator)
-#pt.x.get_root()
+x.prune(x.get_root(),3)
+x.prune(x.get_root(),17)
+pt = PrettyPrintTree(lambda z: z._children, lambda z: z._order)
+pt(x.get_root())
 
 '''
     def manhattan_ghost(m,g):
